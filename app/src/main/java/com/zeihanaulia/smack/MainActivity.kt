a5fc428f1d17ff91d8c5d1897659ef5911100c5d
Zeihan Aulia <zeihanaulia@gmail.com>
2018-12-16 01:03:54 +0700

    Add login activity UI

--- app/src/main/java/com/zeihanaulia/smack/MainActivity.kt
+++ app/src/main/java/com/zeihanaulia/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.zeihanaulia.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -34,7 +35,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun loginBtnNavClicked(view: View){}
+    fun loginBtnNavClicked(view: View){
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
+    }
 
     fun addChannelClicked(view: View){}
 
